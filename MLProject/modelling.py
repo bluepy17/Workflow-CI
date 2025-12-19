291cadd90d919aa142d2a50c79f9fe0bb9432edb
bluepy17 <bluearthpy17@gmail.com>
2025-12-20 03:51:14 +0700

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -27,9 +27,8 @@ warnings.filterwarnings("ignore")
 
 tracking_uri = os.getenv("MLFLOW_TRACKING_URI", "file:./mlruns")
 mlflow.set_tracking_uri(tracking_uri)
-
 mlflow.set_experiment("Heart_Disease_Final_Model")
-mlflow.sklearn.autolog(log_models=False)
+mlflow.sklearn.autolog(disable=True)
 
 
 def load_data():
@@ -71,41 +70,39 @@ def train_model(X_train, X_test, y_train, y_test):
 
     model.fit(X_train, y_train)
 
-  
-    with mlflow.start_run(nested=True):
-        mlflow.sklearn.log_model(model, artifact_path="model")
+    mlflow.sklearn.log_model(model, artifact_path="model")
 
-        y_proba = model.predict_proba(X_test)[:, 1]
-        y_pred = (y_proba >= 0.25).astype(int)
+    y_proba = model.predict_proba(X_test)[:, 1]
+    y_pred = (y_proba >= 0.25).astype(int)
 
-        print(classification_report(y_test, y_pred))
+    print(classification_report(y_test, y_pred))
 
-        metrics = {
-            "accuracy": accuracy_score(y_test, y_pred),
-            "precision": precision_score(y_test, y_pred),
-            "recall": recall_score(y_test, y_pred),
-            "f1_score": f1_score(y_test, y_pred),
-            "roc_auc": roc_auc_score(y_test, y_proba),
-            "threshold": 0.25
-        }
+    metrics = {
+        "accuracy": accuracy_score(y_test, y_pred),
+        "precision": precision_score(y_test, y_pred),
+        "recall": recall_score(y_test, y_pred),
+        "f1_score": f1_score(y_test, y_pred),
+        "roc_auc": roc_auc_score(y_test, y_proba),
+        "threshold": 0.25
+    }
 
-        with open("metric_info.json", "w") as f:
-            json.dump(metrics, f, indent=4)
+    with open("metric_info.json", "w") as f:
+        json.dump(metrics, f, indent=4)
 
-        mlflow.log_artifact("metric_info.json")
-        os.remove("metric_info.json")
+    mlflow.log_artifact("metric_info.json")
+    os.remove("metric_info.json")
 
-        cm = confusion_matrix(y_test, y_pred)
-        plt.figure(figsize=(6, 5))
-        sns.heatmap(cm, annot=True, fmt="d", cmap="Blues")
-        plt.xlabel("Predicted")
-        plt.ylabel("Actual")
-        plt.tight_layout()
-        plt.savefig("training_confusion_matrix.png")
-        plt.close()
+    cm = confusion_matrix(y_test, y_pred)
+    plt.figure(figsize=(6, 5))
+    sns.heatmap(cm, annot=True, fmt="d", cmap="Blues")
+    plt.xlabel("Predicted")
+    plt.ylabel("Actual")
+    plt.tight_layout()
+    plt.savefig("training_confusion_matrix.png")
+    plt.close()
 
-        mlflow.log_artifact("training_confusion_matrix.png")
-        os.remove("training_confusion_matrix.png")
+    mlflow.log_artifact("training_confusion_matrix.png")
+    os.remove("training_confusion_matrix.png")
 
 
 def main():
